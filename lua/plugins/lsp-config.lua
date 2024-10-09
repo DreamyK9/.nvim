175b76005aec73bfc83cbf7d3b734d5253e18b8b
JWolf <it.wolfx64@gmail.com>
2024-10-09 14:29:37 +0200

    start setting up lsp (9:30 typecraft video 3)

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,26 @@
+local M = {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pylsp" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.pylsp.setup({})
+        end
+    }
+}
+
+return M
