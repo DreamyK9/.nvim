34ee0842028192b5f9e6490cc786ca281a7f355d
JWolf <it.wolfx64@gmail.com>
2024-10-11 17:39:27 +0200

    add none-ls plugin

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+local M = {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+            },
+        })
+    end
+}
+
+return { M }
