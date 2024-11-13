d27fb10ed14f15ffc79a3327f099994ab9b5fbe2
Jonas Wolf <it.wolfx64@gmail.com>
2024-11-13 17:34:43 +0100

    add new telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,8 @@ local M = {
         config = function()
             local builtin = require("telescope.builtin")
 
-            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fh', function() vim.cmd("Telescope find_files hidden=true") end, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         end
     },
