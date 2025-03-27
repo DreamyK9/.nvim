dd929c7a2a19abbcc9cdf484d273d5abaec7c76f
JWolf <jonas.wolf@hhi-extern.fraunhofer.de>
2025-03-27 19:36:41 +0100

    start of implementing mode split

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
--- require("wolf.settings")
--- require("wolf.maps")
--- require("wolf.lazy")
+-- choose which config to load
+-- Possible Choices:
+-- "wolf" - custom config by me
+local config = "wolf"
 
-require("wolf")
+-- Choose which mode to load the config in.
+-- Leave empty if not applicable
+-- Possible Choices:
+--    "full" - all features
+--    "basic" - features that are reasonable everywhere
+--    "minimal" - maximum compatibility
+--    "offline" - no downloads needed, only settings
+config_mode = ""
+
+require(config)
--- lua/wolf/init.lua
+++ lua/wolf/init.lua
@@ -1,3 +1,21 @@
+modes = {
+	full = true,
+	basic = true,
+	minimal = true,
+	offline = true,
+}
+
+-- mode is defined globally in the entry file
+if mode == "" then mode = "full"
+
+for m, _ in modes.pairs() do
+	if not m == mode then
+		modes[m] = false
+	else
+		break
+	end
+end
+
 local wolf = {}
 
 wolf.settings = require("wolf.settings")
