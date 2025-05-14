e53c558855993a3ebbb10f0714a1fe328ed2c340
Wolf <jonas.wolf@hhi-extern.fraunhofer.de>
2025-05-14 14:01:09 +0200

    chore: cleanup init

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
--- require("wolf.settings")
--- require("wolf.maps")
--- require("wolf.lazy")
-
 if vim.loop.os_uname().sysname == "Windows_NT" then
 	-- python interpreter. might need to be changed per system
-	-- TODO: Not working yet. NeoVim still says it can't use the interpreter.
 	vim.g.python3_host_prog = vim.env.USERPROFILE .. "/.pyenv/pyenv-win/versions/3.13.3/python.exe"
 end
 
